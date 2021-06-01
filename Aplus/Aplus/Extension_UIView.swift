7a1f0e917ca689dc8f5a3673f7aaf96e4bf704c6
hsh <tmdgus2568@gmail.com>
2021-06-01 15:00:09 +0900

    로그인 화면

---
+++ Aplus/Aplus/Extension_UIView.swift
@@ -0,0 +1,42 @@
+//
+//  Extension_UIView.swift
+//  Aplus
+//
+//  Created by hsh on 2021/06/01.
+//
+// storyboard에 View 부분에서 테두리 둥글게 가능하게 하기 위한 파일 
+
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+    
+}
--- Aplus/Aplus/ViewController.swift
+++ Aplus/Aplus/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isCheckedAuto:Bool! = true;
 
+    @IBOutlet weak var btnAutoLogin: UIButton!
+    @IBOutlet weak var btnLogin: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+  
     }
 
+    // 자동 로그인 체크박스
+    @IBAction func didTapAutoLogin(_ sender: UIButton) {
+        if isCheckedAuto{
+            sender.setImage(UIImage(systemName: "square"), for: .normal)
+            
+            isCheckedAuto = false
+        }
 
+        else{
+            sender.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+            isCheckedAuto = true
+        }
+    }
+    
 }
 
